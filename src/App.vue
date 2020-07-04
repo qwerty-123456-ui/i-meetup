cb78a47d4e00a13a007e4aafa3adac9561fbcce9
qwerty935 <isha26701@gmail.com>
2020-07-04 13:05:29 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,86 @@
 <template>
   <v-app>
     <v-toolbar class="primary">
-      <v-toolbar-side-icon @click="sideNav=!sideNav"  class="hidden-sm-and-up "><span class="mdi mdi-format-align-justify"></span></v-toolbar-side-icon>
-      <span class="space"> </span>
+      <v-toolbar-side-icon @click="sideNav=!sideNav"  class="hidden-sm-and-up"><span class="mdi mdi-format-align-justify"></span></v-toolbar-side-icon>
+      <span class="space"></span>
       <v-toolbar-title>IMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <span class="mdi mdi-48px mdi-account-multiple"></span>
-        <span class="space"> </span>
-        <v-btn flat class="primary">View Meetups</v-btn>
+      <v-toolbar-items class="hidden-xs-only" >
+        <v-btn flat class="primary">
+          <span class="mdi mdi-48px mdi-account-multiple"></span>
+          <span class="space"> </span>
+          VIEW MEETUPS
+        </v-btn>
+        <v-btn flat class="primary">
+          <span class="mdi mdi-48px mdi-map-marker"></span>
+          <span class="space"> </span>
+          ORGANIZE MEETUPS
+        </v-btn>
+        <v-btn flat class="primary">
+          <span class="mdi mdi-48px mdi-account"></span>
+          <span class="space"> </span>
+          PROFILE
+        </v-btn>
+        <v-btn flat class="primary">
+          <span class="mdi mdi-48px mdi-account-circle"></span>
+          <span class="space"> </span>
+          SIGN UP
+        </v-btn>
+        <v-btn flat class="primary">
+          <span class="mdi mdi-48px mdi-face"></span>
+          <span class="space"> </span>
+          SIGN IN
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer v-model="sideNav" >
       <v-list>
         <v-list-tile>
-          <v-list-tile-action v-for="item in menuItems" :key="item.title">
-            <v-toolbar-side-icon @click="sideNav=!sideNav">
-            {{ item.icons }}
-            <span class="mdi mdi-48px mdi-account-multiple"></span>
-            </v-toolbar-side-icon>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account-multiple"></span>
           </v-list-tile-action>
           <v-list-tile-content>
-            {{item.title}}
+            VIEW MEETUPS
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-map-marker"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            ORGANIZE MEETUPS
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            PROFILE
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account-circle"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            SIGN UP
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-face"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            SIGN IN
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -34,24 +93,15 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
-    // HelloWorld
   },
 
   data: () => ({
-    sideNav: false,
-    menuItems:[
-      {icons:<span class="mdi mdi-48px mdi-account-multiple"></span>,title:'VIEW MEETUPS'},
-      {icons:<span class="mdi mdi-map-marker"></span>,title:'ORGANIZE MEETUPS'},
-      {icons:<span class="mdi mdi-account"></span>,title:'PROFILE'},
-      {icons:<span class="mdi mdi-account-circle"></span>,title:'SIGN UP'},
-      {icons:<span class="mdi mdi-face"></span>,title:'SIGN IN'}
-    ]
+    sideNav: false
   }),
 };
 </script>
