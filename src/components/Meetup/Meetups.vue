7c7ed36c7fbbc3d8d9294aab447ac025c359167e
qwerty935 <isha26701@gmail.com>
2020-07-05 12:14:05 +0530

    updated

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -2,16 +2,22 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs12>
-        <v-card>
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <v-container fluid>
-            <v-layout xs5>
-              <v-avatar class="ma-2" size="250" tile>
-                <v-img src="http://1.bp.blogspot.com/-HxaFxmB8kZg/T89-RJdUXGI/AAAAAAAADyo/bfD336mdNbQ/s1600/Manhattan+New+York+City+8.jpg',id:'wqasdfg6we0t67veq3wfy"></v-img>
-              </v-avatar>
-            </v-layout>
-          </v-container>
-        </div>
+        <v-card class="info">
+          <!-- <div class="d-flex flex-no-wrap justify-space-between"> -->
+            <v-container fluid>
+              <v-layout xs5 sm4 md3>
+                <v-avatar class="ma-2" size="250" tile>
+                  <v-img
+                    src="http://1.bp.blogspot.com/-HxaFxmB8kZg/T89-RJdUXGI/AAAAAAAADyo/bfD336mdNbQ/s1600/Manhattan+New+York+City+8.jpg',id:'wqasdfg6we0t67veq3wfy"
+                  ></v-img>
+                </v-avatar>
+                <v-flex xs7 sm8 md9>
+                    <v-card-title primary-title class="white--text"> <h2>MY MEETUP</h2> </v-card-title>
+                    <v-card-title primary-title class="white--text"><h4>17th July</h4></v-card-title>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          <!-- </div> -->
         </v-card>
       </v-flex>
     </v-layout>
