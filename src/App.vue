e369e0221b198ba46f77bb20092cc36274276a53
qwerty935 <isha26701@gmail.com>
2020-07-05 11:04:37 +0530

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,110 @@
 <template>
- <div id="app">
-    <v-app>
-      <v-toolbar class="primary">
-        <v-toolbar-side-icon @click="sideNav=!sideNav"  class="hidden-sm-and-up"><span class="mdi mdi-format-align-justify"></span></v-toolbar-side-icon>
+  <v-app>
+    <v-card color="grey lighten-4" flat height="75px" tile>
+      <v-toolbar extended extension-height="10" class="primary">
+        <v-toolbar-side-icon @click.stop="sideNav= !sideNav" class="hidden-sm-and-up">
+          <span class="mdi mdi-format-align-justify"></span>
+        </v-toolbar-side-icon>
         <span class="space"></span>
         <v-toolbar-title>
           <router-link to="/" tag="span" style="cursor: pointer">IMeetup</router-link>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-xs-only" >
+        <v-toolbar-items class="hidden-xs-only">
           <v-btn flat class="primary">
             <span class="mdi mdi-48px mdi-account-multiple"></span>
-            <span class="space"> </span>
-              <router-link to="/meetups" tag="span" style="cursor: pointer">VIEW MEETUPS</router-link>
+            <span class="space"></span>
+            <router-link to="/meetups" tag="span" style="cursor: pointer">VIEW MEETUPS</router-link>
           </v-btn>
           <v-btn flat class="primary">
             <span class="mdi mdi-48px mdi-map-marker"></span>
-            <span class="space"> </span>
+            <span class="space"></span>
             <router-link to="/meetup/new" tag="span" style="cursor: pointer">ORGANIZE MEETUPS</router-link>
           </v-btn>
           <v-btn flat class="primary">
             <span class="mdi mdi-48px mdi-account"></span>
-            <span class="space"> </span>
+            <span class="space"></span>
             <router-link to="/profile" tag="span" style="cursor: pointer">PROFILE</router-link>
           </v-btn>
           <v-btn flat class="primary">
             <span class="mdi mdi-48px mdi-account-circle"></span>
-            <span class="space"> </span>
+            <span class="space"></span>
             <router-link to="/signup" tag="span" style="cursor: pointer">SIGN UP</router-link>
           </v-btn>
           <v-btn flat class="primary">
             <span class="mdi mdi-48px mdi-face"></span>
-            <span class="space"> </span>
+            <span class="space"></span>
             <router-link to="/signin" tag="span" style="cursor: pointer">SIGN IN</router-link>
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-navigation-drawer temporary v-model="sideNav" >
-        <v-list>
-          <v-list-tile style="cursor : pointer">
-            <v-list-tile-action>
-              <span class="mdi mdi-24px mdi-account-multiple"></span>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <router-link to="/meetups" tag="span" style="cursor: pointer">VIEW MEETUPS</router-link>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <span class="mdi mdi-24px mdi-map-marker"></span>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <router-link to="/meetup/new" tag="span" style="cursor: pointer">ORGANIZE MEETUPS</router-link>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <span class="mdi mdi-24px mdi-account"></span>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <router-link to="/profile" tag="span" style="cursor: pointer">PROFILE</router-link>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <span class="mdi mdi-24px mdi-account-circle"></span>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <router-link to="/signup" tag="span" style="cursor: pointer">SIGN UP</router-link>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <span class="mdi mdi-24px mdi-face"></span>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <router-link to="/signin" tag="span" style="cursor: pointer">SIGN IN</router-link>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <main>
-        <router-view/>
-      </main>
-
-    </v-app>
-  </div>
+    </v-card>
+    <v-navigation-drawer absolute bottom temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account-multiple"></span>
+          </v-list-tile-action>
+          <v-list-tile-content style="cursor: pointer;">
+            <router-link to="/meetups" tag="span">VIEW MEETUPS</router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-18px mdi-map-marker"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <router-link to="/meetup/new" tag="span" style="cursor: pointer">ORGANIZE MEETUPS</router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <router-link to="/profile" tag="span" style="cursor: pointer">PROFILE</router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-account-circle"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <router-link to="/signup" tag="span" style="cursor: pointer">SIGN UP</router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="mdi mdi-24px mdi-face"></span>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <router-link to="/signin" tag="span" style="cursor: pointer">SIGN IN</router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+      <router-view />
+    </main>
+  </v-app>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-  },
+  name: "App",
+  components: {},
   data: () => ({
     sideNav: false
   })
-}
+};
 </script>
 
 <style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,65 @@
 <template>
-    <div>
-        <p>The Home Page</p>
-    </div>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="mb-2">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-btn large router to="/meetup/new" class="info">Organize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-flex>
+        <p class="space"></p>
+    </v-flex>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :key="meetup.id"
+            :src="meetup.imageUrl"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+           <div class="title">
+              {{ meetup.title }}
+          </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-4">
+      <v-flex xs12>
+        Join our awesome meetups!
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+            { imageUrl:'http://1.bp.blogspot.com/-HxaFxmB8kZg/T89-RJdUXGI/AAAAAAAADyo/bfD336mdNbQ/s1600/Manhattan+New+York+City+8.jpg',id:'wqasdfg6we0t67veq3wfy',title: 'Meetup in New York' },
+            { imageUrl:'https://africana.arizona.edu/sites/africana.arizona.edu/files/Eiffel-Tower-Paris-France.jpg',id:'we67yfcw0migectadb',title: 'Meetup in Paris' }      
+        ]
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.space{
+    padding-top: 2px;
+    padding-bottom :2px;
+}
+.title{
+    position: absolute;
+    bottom: 50px;
+    background-color:rgba(0, 0,0, 0.5);
+    color: fff;
+    font-size: 15em;
+    margin-left: 41%;
+}
+</style>
